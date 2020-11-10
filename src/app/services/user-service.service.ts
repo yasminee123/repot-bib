e5b774c71732a379d1ef9e49173a0462168cc8e1
yasminee12 <yasminegoubantini6@gmail.com>
2020-11-10 22:03:07 +0100

    I added addUserUrl and the method add user who take an user in param

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -5,9 +5,19 @@ import {HttpClient} from '@angular/common/http';
 })
 export class UserServiceService {
 
+  private addUserUrl = 'http://localhost:3000/add';
+  
   constructor(private http:HttpClient) { }
-  getAllUsers(){
+ 
+  /*getAllUsers(){
     let data=this.http.get<any>("https://jsonplaceholder.typicode.com/todos")
+    return data;
+  }*/
+
+//Ajouter un utilisateur,cette méthode prend en paramétre l'objet user qu'on lui associera une classe ultérieurement
+  addUser(user) {
+    //Cette méthode utilise le REST API post à l'aide du protocole http
+    let data = this.http.post<any>(this.addUserUrl, user);
     return data;
   }
 }
